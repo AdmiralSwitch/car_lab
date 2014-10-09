35ac71ea32389640611a57159c3b11e36365c5bb
Switch-Thomas <admiralswitch@gmail.com>
2014-10-09 09:11:48 -0700

    complete

--- spec/test.js
+++ spec/test.js
@@ -7,89 +7,107 @@ expect = require('chai').expect;
 describe('Car', function(){
 
   beforeEach(function(){
+    // var today = new Date().getFullYear();
+  
     // create a new myCar object each time
+    myCar = new Car();
   });
 
   describe('#year', function(){
     //think about using the new Date() and getFullYear functions
     it('should be the current year', function(){
-
+      expect(myCar.year).to.equal(2014);
     });
   });
 
   describe('#state', function(){
     it('should initially be off', function(){
-
+      expect(myCar.state).to.equal("off");
     });
   });
 
   describe('#previousOwners', function(){
     it('should initially be empty', function(){
-
+      expect(myCar.previousOwners).to.be.empty;
     });
   });
 
   describe('#curretOwner', function(){
     it('should initially be manufacturer', function(){
-
+      expect(myCar.currentOwner).to.eql("Honda");
     });
   });
 
   describe('#passengers', function(){
     it('should initially be empty', function(){
-
+      expect(myCar.passengers).to.be.empty;
     });
   });
 
   describe('#sale', function(){
 
+    
     it('should move currentOwner to previousOwners array', function(){
-
+      myCar.sale("Bill");
+      expect("Honda").to.eql(myCar.previousOwners[0]);
     });
 
     it('should update currentOwner with the new owner', function(){
-
+      myCar.sale("Bill");
+      expect(myCar.currentOwner).to.eql("Bill");
     });
   });
 
   describe('#paint', function(){
     it('should update the color of myCar', function(){
-
+      myCar = new Car("make", "model", "red");
+      myCar.paint("blue");
+      expect(myCar.color).to.eql("blue");
     });
   });
 
   describe('#start', function(){
     it('should update the state to on', function(){
-
+      myCar.start();
+      expect(myCar.state).to.eql("on");
     });
   });
 
   describe('#off', function(){
     it('should update the state to off', function(){
-
+      myCar.off();
+      expect(myCar.state).to.eql("off");
     });
   });
 
   describe('#park', function(){
     it('should make sure to only work when the car is off', function(){
-
+      myCar.off();
+      expect(myCar.park()).to.eql(true);
     });
 
   });
 
   describe('#pickUp', function(){
     it('should add the passenger to the passengers array if car is on', function(){
-
+      myCar.start();
+      myCar.pickUp("carl");
+      // expect(myCar.passengers).to.contain("carl");
+      expect(myCar.passengers.length).to.equal(1)
+      // console.log(myCar.passengers);
     });
 
     it('should not modify the passengers array if car is off', function(){
-
+      myCar.off();
+      myCar.passengers = [];
+      myCar.pickUp("carl");
+      expect(myCar.passengers).to.be.empty;
     });
   });
 
   describe('#dropOff', function(){
     it('should remove passenger from the passengers array if car is on', function(){
-
+      
     });
 
     it('should leave passenger in the passengers array if car is off', function(){
--- src/car.js
+++ src/car.js
@@ -1,13 +1,47 @@
-function Car(make, model, year, color){
-
+function Car(make, model,color){
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.year = new Date().getFullYear();
+	this.state = "off";
+	this.previousOwners = [];
+	this.currentOwner = "Honda";
+	this.passengers = [];
+	this.park = function(){
+		if (this.state === "off"){
+			return true;
+		} else {
+			return false;
+		}
+	};
 }
+Car.prototype.dropoff = function(name){
+  if (this.state === "on" && this.passengers.indexOf(name) != -1){
+    this.passengers.splice(this.passengers.indexOf(name),1);
+  }
+};
+
+Car.prototype.pickUp = function(name){
+  if (this.state === "on"){
+    this.passengers.push(name);
+  } 
+};
 
 Car.prototype.sale = function(newOwner){
-
+	this.previousOwners.push(this.currentOwner);
+	this.currentOwner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
+	this.color = newColor;
 
+};
+
+Car.prototype.start = function(){
+	this.state = "on";
+};
+Car.prototype.off = function(){
+	this.state = "off";
 };
 
 module.exports=Car;
